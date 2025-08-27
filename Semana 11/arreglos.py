ff6acfbc7161703a72de00aff2ab0b00e716f210
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2025-08-27 18:51:44 -0500

    deber_semana11

---
+++ Semana 11/arreglos.py
@@ -0,0 +1,11 @@
+matriz = [
+[1,2,3],
+[2,4,5],
+[3,7,8]
+]
+
+print(matriz)
+for filas in matriz:
+    for columnas in filas:
+        if columnas == 8:
+            print("Si existe el numero 8 en l amatriz")
