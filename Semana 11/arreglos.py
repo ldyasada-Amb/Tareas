b9e3b9e6ffe9a717003f0ff24e4069ada1bb7d24
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2025-08-31 21:45:38 -0500

    hola

--- Semana 11/arreglos.py
+++ Semana 11/arreglos.py
@@ -8,6 +8,5 @@ print(matriz)
 for filas in matriz:
     for columnas in filas:
         if columnas == 8:
-            print("Si existe el numero 8 en l amatriz")
+            print("Si existe el numero 8 en la matriz")
             
-print("nnongs")
