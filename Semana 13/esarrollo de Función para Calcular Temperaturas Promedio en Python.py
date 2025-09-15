512211aa965eddcf85fd2e6f358b90ddd8bb28fc
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2025-09-14 19:27:56 -0500

    holaaaaa

---
+++ Semana 13/esarrollo de Función para Calcular Temperaturas Promedio en Python.py
@@ -0,0 +1,40 @@
+temperaturas = [
+    [{"lunes":17,  #Ciudad 1
+     "martes":18,
+     "miercoles":17,
+     "jueves":20,
+     "viernes":18,
+     "sabado":16,
+     "domingo":19}
+],
+
+    [{"lunes":17,   #Ciudad 2
+     "martes":16,
+     "miercoles":19,
+     "jueves":20,
+     "viernes":18,
+     "sabado":16,
+     "domingo":15}
+     ],
+     [{"lunes":17, #ciudad 3
+     "martes":19,
+     "miercoles":19,
+     "jueves":20,
+     "viernes":18,
+     "sabado":17,
+     "domingo":16}],
+]
+
+#print(temperaturas[1][1])
+def promedio_temperatura (promedio):
+    promedio = 0
+    for capas in temperaturas:
+        for clave,valor in capas[0].items():
+                promedio += valor
+        print( "temperatura promedio", promedio/7)
+        promedio = 0
+        print("+"*20)
+    return promedio
+        
+valor = promedio_temperatura(temperaturas)
+
